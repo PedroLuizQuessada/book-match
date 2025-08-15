161fa98fb2789e5ca9e2e4f3257ed7ce8e2a81c7
Quessada <poqs@gft.com>
2025-08-14 23:15:59 -0300

    feat: remember me button

--- app/src/main/java/com/example/bookmatch/ui/Login.kt
+++ app/src/main/java/com/example/bookmatch/ui/Login.kt
@@ -4,15 +4,18 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.CheckBox
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import com.example.bookmatch.R
 import com.example.bookmatch.data.Users
+import androidx.core.content.edit
 
 class Login : AppCompatActivity() {
     private lateinit var email: EditText
     private lateinit var password: EditText
+    private lateinit var rememberMe: CheckBox
     private lateinit var loginButton: Button
     private lateinit var signUpLink: TextView
     private lateinit var forgotPassword: TextView
@@ -21,8 +24,14 @@ class Login : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        val preferences = getSharedPreferences("remember_me", MODE_PRIVATE)
+        if (preferences.contains("e_mail")) {
+            redirect()
+        }
+
         email = findViewById(R.id.email)
         password = findViewById(R.id.password)
+        rememberMe = findViewById(R.id.remember_me)
         loginButton = findViewById(R.id.login_button)
         signUpLink = findViewById(R.id.signup)
         forgotPassword = findViewById(R.id.forgot_password)
@@ -30,8 +39,8 @@ class Login : AppCompatActivity() {
         loginButton.setOnClickListener {
             try {
                 Users.getUser(email.text.toString(), password.text.toString())
-                val intent = Intent(this, Home::class.java)
-                startActivity(intent)
+                configureRememberMe()
+                redirect()
             }
             catch (e: Exception) {
                 Toast.makeText(this, e.message, Toast.LENGTH_SHORT).show()
@@ -48,6 +57,23 @@ class Login : AppCompatActivity() {
             val forgotPasswordDialog = ForgotPasswordDialog(this, R.style.DialogTheme)
             forgotPasswordDialog.window?.setBackgroundDrawableResource(android.R.color.transparent)
             forgotPasswordDialog.show()
+        }
+    }
+
+    private fun redirect() {
+        val intent = Intent(this, Home::class.java)
+        startActivity(intent)
+    }
+
+    private fun configureRememberMe() {
+        val preferences = getSharedPreferences("remember_me", MODE_PRIVATE)
+        preferences.edit {
+            if (rememberMe.isChecked) {
+                putString("e_mail", email.text.toString())
+            } else {
+                remove("e_mail")
+            }
+
         }
     }
 }
